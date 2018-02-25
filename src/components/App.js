3df438b1624c1f0ff7abf0addaad904389fc44dc
Peter Krevenets <peter.k.nets@gmail.com>
2018-02-25 12:21:12 +0200

    Change StatusBar into InfoBar and show some social icons

--- src/components/App.js
+++ src/components/App.js
@@ -2,39 +2,39 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import StatusBar from "./StatusBar";
+import InfoBar from "./InfoBar";
 import Gameboard from "./Gameboard";
 import GameEngine from "./GameEngine";
 import Interface from "./Interface";
 import * as actions from "../actions";
 
 class App extends Component {
-  componentDidMount() {
-    // const minSquaresH = 16;
-    // const minSquaresV = 8;
-    const minSquareSize = 20;
-    const windowHeight = window.innerHeight;
-    const windowWidth = window.innerWidth;
-    const windowMinSide = Math.min(windowHeight, windowWidth);
-    const maxSquaresInMinSide = Math.trunc(windowMinSide / minSquareSize);
-    // const maxSquaresH = Math.trunc(windowWidth / 38);
-    const squareSide = windowMinSide / maxSquaresInMinSide;
-    const avaiableWindowHeight = windowHeight - windowMinSide;
-    // const maxSquaresV = Math.trunc(avaiableWindowHeight / squareSide);
-    // const verticalSpaceLeft = avaiableWindowHeight - minSquaresV * squareSide;
+  // componentDidMount() {
+  // const minSquaresH = 16;
+  // const minSquaresV = 8;
+  // const minSquareSize = 20;
+  // const windowHeight = window.innerHeight;
+  // const windowWidth = window.innerWidth;
+  // const windowMinSide = Math.min(windowHeight, windowWidth);
+  // const maxSquaresInMinSide = Math.trunc(windowMinSide / minSquareSize);
+  // const maxSquaresH = Math.trunc(windowWidth / 38);
+  // const squareSide = windowMinSide / maxSquaresInMinSide;
+  // const avaiableWindowHeight = windowHeight - windowMinSide;
+  // const maxSquaresV = Math.trunc(avaiableWindowHeight / squareSide);
+  // const verticalSpaceLeft = avaiableWindowHeight - minSquaresV * squareSide;
 
-    // document.querySelector(
-    //   ".status-bar"
-    // ).style.paddingBottom = `${verticalSpaceLeft + 4}px`;
+  // document.querySelector(
+  //   ".status-bar"
+  // ).style.paddingBottom = `${verticalSpaceLeft + 4}px`;
 
-    // console.log("window.innerHeight", windowHeight);
-    // console.log("window.innerWidth", windowWidth);
-    // console.log("max horizontal squares", maxSquaresH);
-    console.log("number of squares", maxSquaresInMinSide);
-    console.log("available window height", avaiableWindowHeight);
-    // console.log("max vertical squares", maxSquaresV);
-    // console.log("vertical space left", verticalSpaceLeft);
-  }
+  // console.log("window.innerHeight", windowHeight);
+  // console.log("window.innerWidth", windowWidth);
+  // console.log("max horizontal squares", maxSquaresH);
+  // console.log("number of squares", maxSquaresInMinSide);
+  // console.log("available window height", avaiableWindowHeight);
+  // console.log("max vertical squares", maxSquaresV);
+  // console.log("vertical space left", verticalSpaceLeft);
+  // }
 
   handleMouseUp = () => {
     this.props.dragMouse(false);
@@ -45,13 +45,12 @@ class App extends Component {
 
     return (
       <div className="app" onMouseUp={this.handleMouseUp}>
-        {/* <StatusBar /> */}
         <div className="element-before">
           <Interface />
         </div>
         {isPlaying && <GameEngine />}
         <Gameboard />
-        <div className="element-after" />
+        <InfoBar />
       </div>
     );
   }
---
+++ src/components/InfoBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="info-bar">
+      <a href="https://github.com/GuRuGuMaWaRu" target="blank">
+        <i className="info-bar__icon ion-social-github" />
+      </a>
+      <a href="https://twitter.com/peter_krevenets" target="blank">
+        <i className="info-bar__icon ion-social-twitter" />
+      </a>
+    </div>
+  );
+};
