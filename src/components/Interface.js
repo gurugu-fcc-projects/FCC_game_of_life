d7847931a30bb70b85b40c96098cdf9749277a2c
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-02 11:55:41 +0200

    Uninstall material-ui & start creating drawer

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+
 import * as actions from "../actions";
 
 const Interface = ({ isPlaying, startGame, stopGame }) => {
@@ -12,7 +13,19 @@ const Interface = ({ isPlaying, startGame, stopGame }) => {
     }
   };
 
-  return <div className="interface">hggh</div>;
+  return (
+    <div className="interface">
+      <button className="interface__button button-1" title="button">
+        <i className="interface__icon ion-ios-keypad" />
+      </button>
+      <button className="interface__button button-2" title="button">
+        <i className="interface__icon ion-ios-keypad" />
+      </button>
+      <button className="interface__button button-3" title="button">
+        <i className="interface__icon ion-ios-keypad" />
+      </button>
+    </div>
+  );
 };
 
 Interface.propTypes = {
