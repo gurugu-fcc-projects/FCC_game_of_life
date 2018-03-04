2fb7bf4ed53f0bf2f63c2bde849585e122d8dc2c
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-04 08:39:39 +0200

    Add function to close Settings drawer; add styles

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Settings = () => {
-  return <div className="settings">Settings</div>;
+  const handleCloseSettings = () => {
+    document.querySelector(".settings").classList.toggle("open");
+  };
+
+  return (
+    <div className="settings">
+      <button
+        className="settings__button"
+        title="Close"
+        onClick={handleCloseSettings}
+      >
+        <i className="settings__icon ion-arrow-left-c" />
+      </button>
+      <div>
+        <div>Size Slider</div>
+        <div>Speed Slider</div>
+        <div>Color Slider</div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
