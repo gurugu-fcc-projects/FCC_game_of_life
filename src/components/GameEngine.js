df76459e88730c9efa4e35f5625295375a7f866a
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-04 10:23:26 +0200

    Move check dead-alive functionality into an updateGameboard utility file

--- src/components/GameEngine.js
+++ src/components/GameEngine.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+
 import * as actions from "../actions";
+import { checkDeadAlive } from "../utils/updateGameboard";
 
 class GameEngine extends Component {
   componentWillUpdate() {
@@ -20,46 +22,15 @@ class GameEngine extends Component {
     }
   }
 
-  isAlive = (cell, gameboard) => {
-    const aliveNeighbours = cell.neighbours.filter(
-      neighbour => gameboard[neighbour].alive
-    ).length;
-
-    if (cell.alive && (aliveNeighbours === 2 || aliveNeighbours === 3)) {
-      return true;
-    }
-    if (!cell.alive && aliveNeighbours === 3) {
-      return true;
-    }
-
-    return false;
-  };
-
-  checkGameboard = gameboard => {
-    //=== update gameboard with new dead/alive status
-    const updatedGameboard = {};
-
-    for (let cell in gameboard) {
-      updatedGameboard[cell] = {
-        alive: this.isAlive(gameboard[cell], gameboard),
-        neighbours: gameboard[cell].neighbours
-      };
-    }
-
-    this.props.runGame(updatedGameboard);
-  };
-
   componentDidMount() {
-    console.log("Playing - componentDidMount");
     const interval = setInterval(
-      () => this.checkGameboard(this.props.gameboard),
+      () => this.props.runGame(checkDeadAlive(this.props.gameboard)),
       1000
     );
     this.props.setIntervalId(interval);
   }
 
   componentWillUnmount() {
-    console.log("clearing interval");
     clearInterval(this.props.intervalId);
   }
 
