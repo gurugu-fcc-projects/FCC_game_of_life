02483f442545d6a5fce7275e956227fc008244de
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-05 13:45:47 +0200

    Add Tests - add-remove Alive class on mousedrag

--- cypress/integration/main__basics.js
+++ cypress/integration/main__basics.js
@@ -18,17 +18,53 @@ describe("Game of Life app", () => {
       cy.visit("http://localhost:3000");
     });
 
-    it("adds Alive class to a Dead cell on click", () => {
+    it("adds Alive class when clicking on Dead cell", () => {
       cy
         .get(".0-0")
         .click()
         .should("have.class", "alive");
     });
-    it("removes Alive class from an Alive cell on click", () => {
+    it("removes Alive class when clicking on Alive cell", () => {
       cy
         .get(".0-0")
         .click()
         .click()
+        .should("not.have.class", "alive");
+    });
+    it("adds Alive class on mouse drag on Dead cells", () => {
+      cy
+        .get(".0-0")
+        .trigger("mousedown")
+        .should("have.class", "alive");
+      cy
+        .get(".0-1")
+        .trigger("mouseover")
+        .should("have.class", "alive");
+      cy
+        .get(".0-2")
+        .trigger("mouseover")
+        .should("have.class", "alive");
+    });
+    it("removes Alive class on mouse drag on Alive cells", () => {
+      cy
+        .get(".0-0")
+        .trigger("mousedown")
+        .should("have.class", "alive");
+      cy
+        .get(".0-1")
+        .trigger("mouseover")
+        .should("have.class", "alive");
+      cy
+        .get(".0-2")
+        .trigger("mouseover")
+        .should("have.class", "alive");
+      cy
+        .get(".0-1")
+        .trigger("mouseover")
+        .should("not.have.class", "alive");
+      cy
+        .get(".0-0")
+        .trigger("mousedown")
         .should("not.have.class", "alive");
     });
   });
