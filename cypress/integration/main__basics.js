845cdc3941700925f64747da7e593564bbfc4c9b
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-05 12:00:41 +0200

    Add Test - has correct title

--- cypress/integration/main__basics.js
+++ cypress/integration/main__basics.js
@@ -2,4 +2,8 @@ describe("Game of Life app", () => {
   it("successfully loads", () => {
     cy.visit("http://localhost:3000");
   });
+  it("has correct title", () => {
+    cy.visit("/");
+    cy.title().should("be", "Game of Life by GuRuGuMaWaRu");
+  });
 });
