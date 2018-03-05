966b41af0dc522212388b5cd7d875dd3f67bd1c7
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-05 12:13:54 +0200

    Add Tests - add and remove Alive class

--- cypress/integration/main__basics.js
+++ cypress/integration/main__basics.js
@@ -2,8 +2,34 @@ describe("Game of Life app", () => {
   it("successfully loads", () => {
     cy.visit("http://localhost:3000");
   });
-  it("has correct title", () => {
-    cy.visit("/");
-    cy.title().should("be", "Game of Life by GuRuGuMaWaRu");
+
+  context("Layout", () => {
+    beforeEach(function() {
+      cy.visit("http://localhost:3000");
+    });
+
+    it("has correct title", () => {
+      cy.title().should("be", "Game of Life by GuRuGuMaWaRu");
+    });
+  });
+
+  context("Basic functionality", () => {
+    beforeEach(function() {
+      cy.visit("http://localhost:3000");
+    });
+
+    it("adds Alive class to a Dead cell on click", () => {
+      cy
+        .get(".0-0")
+        .click()
+        .should("have.class", "alive");
+    });
+    it("removes Alive class from an Alive cell on click", () => {
+      cy
+        .get(".0-0")
+        .click()
+        .click()
+        .should("not.have.class", "alive");
+    });
   });
 });
