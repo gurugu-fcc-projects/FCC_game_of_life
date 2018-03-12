17e0760a89fcab49a175a8d811554529f3bd3745
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-12 13:41:33 +0200

    Install rc-slider and add rc-sliders into Settings drawer

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,12 +1,32 @@
+import "rc-slider/assets/index.css";
+import "rc-tooltip/assets/bootstrap.css";
 import React, { Fragment } from "react";
+import Tooltip from "rc-tooltip";
+import Slider from "rc-slider";
 
 import { showHideSettings } from "../utils/showHideSettings";
 
-const Settings = () => {
-  console.log("rendering Settings...");
-
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+const Handle = Slider.Handle;
+const handle = props => {
+  const { value, dragging, index, ...restProps } = props;
   return (
-    <div>
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  );
+};
+
+const Settings = () => {
+  return (
+    <Fragment>
       <div id="modal" onClick={showHideSettings} />
       <aside id="settings">
         <button
@@ -16,13 +36,28 @@ const Settings = () => {
         >
           <i className="settings__icon ion-arrow-left-c" />
         </button>
-        <div>
-          <div>Size Slider</div>
-          <div>Speed Slider</div>
-          <div>Color Slider</div>
+        <div className="settings-sliders">
+          <div>
+            <h4>Size Slider</h4>
+            <div>
+              <Slider min={0} max={20} defaultValue={3} handle={handle} />
+            </div>
+          </div>
+          <div>
+            <h4>Speed Slider</h4>
+            <div>
+              <Slider min={0} max={20} defaultValue={3} handle={handle} />
+            </div>
+          </div>
+          <div>
+            <h4>Color Slider</h4>
+            <div>
+              <Slider min={0} max={20} defaultValue={3} handle={handle} />
+            </div>
+          </div>
         </div>
       </aside>
-    </div>
+    </Fragment>
   );
 };
 
