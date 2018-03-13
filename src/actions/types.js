5fa910a4a094a58ccda0a5bf7ba31efd78c9c0fc
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-13 15:03:37 +0200

    Connect Speed to state & slider, start with Color

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const DRAG_MOUSE = "drag_mouse";
 export const START_STOP_GAME = "start_stop_game";
 export const SET_INTERVAL_ID = "set_interval_id";
 export const CHANGE_SIZE = "change_size";
+export const CHANGE_SPEED = "change_speed";
+export const CHANGE_COLOR = "change_color";
