fdab439ebac4851fb959fad503dd79f3002bb7e9
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-14 09:20:33 +0200

    Implement Color Change functionality

--- src/reducers/variables.js
+++ src/reducers/variables.js
@@ -3,17 +3,26 @@ import { CHANGE_SIZE, CHANGE_SPEED, CHANGE_COLOR } from "../actions/types";
 const INIT = {
   size: 20,
   speed: 10,
-  color: 3
+  color: 1
 };
 
 export default function(state = INIT, action) {
   switch (action.type) {
     case CHANGE_SIZE:
-      return { ...state, size: action.payload };
+      return {
+        ...state,
+        size: action.payload
+      };
     case CHANGE_SPEED:
-      return { ...state, speed: action.payload };
+      return {
+        ...state,
+        speed: action.payload
+      };
     case CHANGE_COLOR:
-      return { ...state, color: action.payload };
+      return {
+        ...state,
+        color: action.payload
+      };
     default:
       return state;
   }
