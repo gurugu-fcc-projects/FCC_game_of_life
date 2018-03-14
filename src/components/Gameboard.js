f9a108a6175b76a2eda153955ca0f7a35e574a06
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-14 11:06:37 +0200

    Change Gameboard component from class-based into classless

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
-class Gameboard extends Component {
-  createGameboard = size => {
-    let gameboard = [];
+const Gameboard = ({ gameboard, mouseDrag, size, dragMouse, toggleCell }) => {
+  const createGameboard = size => {
+    let newGameboard = [];
 
     for (let i = 0; i < size; i++) {
       let row = [];
@@ -16,49 +16,43 @@ class Gameboard extends Component {
           <div
             key={j}
             className={`${cellName}${
-              this.props.gameboard[cellName].alive ? " alive" : ""
+              gameboard[cellName].alive ? " alive" : ""
             }`}
           />
         );
       }
 
-      gameboard.push(
+      newGameboard.push(
         <div key={i} className="row">
           {row}
         </div>
       );
     }
 
-    return gameboard;
+    return newGameboard;
   };
 
-  handleMouseDown = ({ target }) => {
-    this.props.dragMouse(true);
-    this.props.toggleCell(target.classList.item(0));
+  const handleMouseDown = ({ target }) => {
+    dragMouse(true);
+    toggleCell(target.classList.item(0));
   };
 
-  handleMouseOver = ({ target }) => {
-    if (this.props.mouseDrag) {
-      this.props.toggleCell(target.classList.item(0));
+  const handleMouseOver = ({ target }) => {
+    if (mouseDrag) {
+      toggleCell(target.classList.item(0));
     }
   };
 
-  render() {
-    const { size } = this.props;
-
-    return (
-      <div
-        onMouseDown={this.handleMouseDown}
-        onMouseOver={this.handleMouseOver}
-        onDragStart={this.handleDragStart}
-        onDragEnd={this.handleDragEnd}
-        className="gameboard"
-      >
-        {this.createGameboard(size)}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      onMouseDown={handleMouseDown}
+      onMouseOver={handleMouseOver}
+      className="gameboard"
+    >
+      {createGameboard(size)}
+    </div>
+  );
+};
 
 Gameboard.propTypes = {
   gameboard: PropTypes.object.isRequired,
