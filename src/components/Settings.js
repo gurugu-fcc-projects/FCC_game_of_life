dbb5f30b70f7f6302720cef06367b39b112844f1
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-14 09:54:19 +0200

    Move sliders into a separate component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,37 +1,11 @@
-import "rc-slider/assets/index.css";
-import "rc-tooltip/assets/bootstrap.css";
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import Slider from "rc-slider";
 
+import SliderComponent from "./Slider";
 import * as actions from "../actions";
 import { showHideSettings } from "../utils/showHideSettings";
 import { colors400 } from "../utils/data";
-
-const sliderStyle = {
-  display: "inline-block",
-  width: "70%"
-};
-
-const trackStyle = {
-  backgroundColor: "rgb(247, 147, 129)",
-  height: 10
-};
-
-const handleStyle = {
-  backgroundColor: "#fff",
-  border: "none",
-  height: 28,
-  width: 28,
-  marginLeft: -14,
-  marginTop: -9
-};
-
-const railStyle = {
-  backgroundColor: "rgb(201, 201, 201)",
-  height: 10
-};
 
 const Settings = ({
   variables: { size, speed, color },
@@ -52,54 +26,27 @@ const Settings = ({
           <i className="icon ion-arrow-left-c" />
         </button>
         <div className="sliders">
-          <div className="slider">
-            <div style={sliderStyle}>
-              <h4>Size</h4>
-              <Slider
-                min={4}
-                max={40}
-                value={size}
-                trackStyle={trackStyle}
-                handleStyle={handleStyle}
-                railStyle={railStyle}
-                onChange={changeSize}
-              />
-            </div>
-            <div className="value">{size}</div>
-          </div>
-          <div className="slider">
-            <div style={sliderStyle}>
-              <h4>Speed</h4>
-              <Slider
-                min={1}
-                max={20}
-                value={speed}
-                trackStyle={trackStyle}
-                handleStyle={handleStyle}
-                railStyle={railStyle}
-                onChange={changeSpeed}
-              />
-            </div>
-            <div className="value">{speed}</div>
-          </div>
-          <div className="slider">
-            <div style={sliderStyle}>
-              <h4>Cell Color</h4>
-              <Slider
-                min={1}
-                max={14}
-                value={color}
-                trackStyle={trackStyle}
-                handleStyle={handleStyle}
-                railStyle={railStyle}
-                onChange={changeColor}
-              />
-            </div>
-            <div
-              className="value color"
-              style={{ backgroundColor: colors400[color] }}
-            />
-          </div>
+          <SliderComponent
+            title="Size"
+            min={4}
+            max={40}
+            value={size}
+            handleChange={changeSize}
+          />
+          <SliderComponent
+            title="Speed"
+            min={1}
+            max={20}
+            value={speed}
+            handleChange={changeSpeed}
+          />
+          <SliderComponent
+            title="Color"
+            min={1}
+            max={14}
+            value={color}
+            handleChange={changeColor}
+          />
         </div>
       </aside>
     </Fragment>
